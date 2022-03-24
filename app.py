1f88071904785f15b1c730e6ca014159b5358bd2
blackmufasa <93259458+blackmufasa@users.noreply.github.com>
2022-03-24 14:21:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os, json, flask
+from  flask_restful import reqparse
 from flask import Flask, request, render_template
 from flask_restful import Resource, Api
 import pandas as pd
@@ -9,6 +10,8 @@ from config import config
 app = Flask(__name__)
 api = Api(app)
 
+sql_parser = reqparse.RequestParser()
+sql_parser.add_argument('limit', type=int, help="Limit the returned rows of SQL Query")
 
 # custom_sql_args = reqparse.RequestParser()
 # custom_sql_args.add_argument("sql", type=str, help="Send SQL Query", required=True)
@@ -27,7 +30,11 @@ def list_api_page():
 @app.route("/fetch_apps/<string:tablename>", methods=['GET'])
 def fetchdata_apps(tablename):
     #tablename='accnt'
-    df = pd.DataFrame(fetch_data(tablename))
+    sql_args = sql_parser.parse_args()
+    if sql_args['limit'] == '' or sql_args['limit'] is None:
+        df = pd.DataFrame(fetch_data(tablename, None))
+    else:
+        df = pd.DataFrame(fetch_data(tablename, sql_args['limit']))
     return flask.jsonify(json.loads(df.to_json(orient='records')))
 
 
@@ -70,7 +77,11 @@ class CustomSql(Resource):
 
     def post(self):
         data = request.get_json()
-        custom_data = fetch_data_custom(data)
+        sql_args = sql_parser.parse_args()
+        if sql_args['limit'] == '' or sql_args['limit'] is None:
+            custom_data = fetch_data_custom(data, None)
+        else:
+            custom_data = fetch_data_custom(data, sql_args['limit'])
         return json.loads(custom_data)
 
 
