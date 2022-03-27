7c6a3b3976f52d287a5439692bdd8340293441cd
blackmufasa <93259458+blackmufasa@users.noreply.github.com>
2022-03-27 18:44:05 +0530

    Update connect_db.py

--- utils/connect_db.py
+++ utils/connect_db.py
@@ -2,7 +2,7 @@ import psycopg2
 from config import config
 
 # Connect ot DB and fetch result set by executing query on DB
-def execute_sql(query, sql_limit):
+def execute_sql(query, sql_args):
     try:
         conn = psycopg2.connect(**config.config)
     except psycopg2.OperationalError as e:
@@ -11,12 +11,10 @@ def execute_sql(query, sql_limit):
     if conn is not None:
         cur = conn.cursor()
         try:
-            cur.execute(query)
+            cur.execute('Select * from ( ' + query + ' ) as AaZz ' + sql_args)
+            #cur.execute(query)
             cols = [desc[0] for desc in cur.description]
-            if sql_limit is None:
-                rows = cur.fetchall()
-            else:
-                rows = cur.fetchmany(sql_limit)
+            rows = cur.fetchall()
             return cols, rows
         except Exception as e:
             print(e)
@@ -28,8 +26,8 @@ def execute_sql(query, sql_limit):
         return "ConnError", "failed"
 
 
-def fetch_resulset(query, sql_limit):
-    ret_col, ret_row = execute_sql(query, sql_limit)
+def fetch_resulset(query, sql_args):
+    ret_col, ret_row = execute_sql(query, sql_args)
     if ret_col == 'SQLError':
         return ret_row, "Could not fetch Results. Failed to execute SQL. Check SQL statement"
     if ret_col == 'ConnError':
