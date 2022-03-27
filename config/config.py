626886921dd8f0a04a5980d362de67415665c640
blackmufasa <93259458+blackmufasa@users.noreply.github.com>
2022-03-27 22:34:25 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -11,7 +11,7 @@ project_sql = os.path.join(BASE_DIR, 'sql')
 NAME = os.environ.get('JDV_SCHEMA')
 USER = os.environ.get('JDV_USER')
 PASSWORD = os.environ.get('JDV_PASSWORD')
-HOST = os.environ.get('JDV_HOST') if os.environ.get('JDV_HOST') is not None else 'jdv.prod.a4.vary.redhat.com'
+HOST = os.environ.get('JDV_HOST') if os.environ.get('JDV_HOST') is not None else 'jdv.dev.a1.vary.redhat.com'
 PORT = os.environ.get('JDV_PORT') if os.environ.get('JDV_PORT') is not None else 35432
 config = {
         'host': HOST,
